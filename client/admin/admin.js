a4ba7ec619c857c6517d2ca187d5f87c018d1906
Jasper Middendorp <Jaspermid@gmail.com>
2015-02-12 21:16:11 +0100

    edited navbar and added switching between implementing and published

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -1,8 +1,20 @@
 /**
  * Created by Jasper on 28/12/14.
  */
+Template.admin.rendered = function() {
+
+    $('.published').addClass("active");
+Session.set ("Published", true)
+
+}
 
 Template.admin.helpers ({
+
+    published: function(){
+      return Session.get('Published')
+    },
+
+
 
     publishedorganisation: function (){
         return Published.find({}, {sort: [
@@ -14,6 +26,16 @@ Template.admin.helpers ({
 
 
 Template.admin.events ({
+
+
+    'click .published': function (){
+        Session.set ("Published", true)
+    },
+
+    'click .implementing': function (){
+        Session.set ("Published", false)
+    },
+
     'click .add': function(){
 
 
@@ -27,8 +49,6 @@ Template.admin.events ({
     },
 
     'click .remove': function(){
-
-
 
         var e = document.getElementById("selectedorganisation");
         var selectedValue = e.options[e.selectedIndex].value;
@@ -47,5 +67,20 @@ Template.admin.events ({
                     score: $('.publishing_org_score_edit').val()
                 }
             });
-    }
+    },
+
+
+    //Implementing organisations
+
+    'click .addimplementing': function(){
+
+
+        Implementing.insert(
+            {
+                "name": $('.implementing_org_name').val(),
+                "score": $('.implementing_org_score').val()
+            }
+
+        )
+    },
 })
--- client/leaderboard/leaderboard.js
+++ client/leaderboard/leaderboard.js
@@ -2,6 +2,16 @@
  * Created by Jasper on 28/12/14.
  */
 
+Template.leaderboard.rendered = function (){
+
+    $('.sort_by_score').addClass("active");
+    Session.set ("sort_by_score", true)
+    $('.published').addClass("active");
+    Session.set ("Published", true)
+
+}
+
+
 Template.leaderboard.helpers ({
     organisation: function () {
         if (Session.get('sort_by_score')) {
@@ -33,4 +43,27 @@ Template.leaderboard.helpers ({
 
 
     }
+
+
 );
+
+Template.leaderboard.events ({
+
+    'click .published': function (){
+        Session.set ("Published", true)
+    },
+
+    'click .implementing': function (){
+        Session.set ("Published", false)
+    },
+
+
+    'click .sort_by_score': function (){
+        Session.set ("sort_by_score", true)
+    },
+
+    'click .sort_by_name': function (){
+        Session.set ("sort_by_score", false)
+    }
+
+})
